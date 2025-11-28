0eea3716c07994a4ec39e1ba0694b291b0fba716
BlackTremolo <mihsee436@gmail.com>
2025-11-28 18:52:08 +0800

    Создание и добавление записи в БД

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do 
+	$db = SQLite3::Database.new 'barbershop.db'
+	$db.execute 'CREATE TABLE if not exists Users (
+	    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+	    name      TEXT,
+	    phone     INTEGER,
+	    datestamp TEXT,
+	    barber    TEXT,
+	    color     TEXT
+	);'
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -25,6 +39,11 @@ post '/visit' do
 	@datestamp = params[:datestamp]
 	@barber = params[:barber]
 	@color = params[:color]
+	
+	$db.execute 'insert into Users 
+	(name, phone, datestamp, barber, color)
+	values (?, ?, ?, ?, ?)', [@username, @phone, @datestamp, @barber, @color]
+
 	
 	hh = {:username => 'Введите имя', :phone => 'Введите телефон', :datetime => 'Введите дату и время'}
 
